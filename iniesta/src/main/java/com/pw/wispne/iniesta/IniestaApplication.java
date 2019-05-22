4d1b459bdbfac8477091b1961bb2da9839b50ffe
lijunyan <li.junyan@microcyber.cn>
2019-05-22 15:48:30 +0800

    愚かなる弟よ......このオレを杀したくば恨め！憎め！そしてみにくく生きのびるがいい.........逃げて...逃げて...生にしがみつくがいい，そして　いつがオレと同じ　眼　をもってオレの前に来い

---
+++ iniesta/src/main/java/com/pw/wispne/iniesta/IniestaApplication.java
@@ -0,0 +1,13 @@
+package com.pw.wispne.iniesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IniestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IniestaApplication.class, args);
+	}
+
+}
