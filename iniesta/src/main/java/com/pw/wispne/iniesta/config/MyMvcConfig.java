ffb001b9649d78d51a36de0db1c750bc41c6fe05
lijunyan <li.junyan@microcyber.cn>
2019-05-28 16:59:32 +0800

    愚かなる弟よ......このオレを杀したくば恨め！憎め！そしてみにくく生きのびるがいい.........逃げて...逃げて...生にしがみつくがいい，そして　いつがオレと同じ　眼　をもってオレの前に来い

--- iniesta/src/main/java/com/pw/wispne/iniesta/config/MyMvcConfig.java
+++ iniesta/src/main/java/com/pw/wispne/iniesta/config/MyMvcConfig.java
@@ -3,9 +3,11 @@ package com.pw.wispne.iniesta.config;
 import com.pw.wispne.iniesta.helloworld.service.HelloService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class MyAppConfig {
+public class MyMvcConfig extends WebMvcConfigurationSupport {
 
     @Bean
     public HelloService helloService(){
