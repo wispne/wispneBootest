c69eee98ebbb602392b24a4eb620609ebbc6fd40
wispne@163.com <54superman>
2019-05-28 20:57:12 +0800

    愚かなる弟よ......このオレを杀したくば恨め！憎め！そしてみにくく生きのびるがいい.........逃げて...逃げて...生にしがみつくがいい，そして　いつがオレと同じ　眼　をもってオレの前に来い

--- iniesta/src/main/java/com/pw/wispne/iniesta/config/MyMvcConfig.java
+++ iniesta/src/main/java/com/pw/wispne/iniesta/config/MyMvcConfig.java
@@ -3,14 +3,17 @@ package com.pw.wispne.iniesta.config;
 import com.pw.wispne.iniesta.helloworld.service.HelloService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MyMvcConfig extends WebMvcConfigurationSupport {
 
-    @Bean
-    public HelloService helloService(){
-        return new HelloService();
+    @Override
+    protected void addViewControllers(ViewControllerRegistry registry) {
+        //super.addViewControllers(registry);
+        registry.addViewController("/caonimabi").setViewName("success");
+
     }
 }
