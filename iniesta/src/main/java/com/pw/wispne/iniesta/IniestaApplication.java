c0c9276b133dd6537ae258cb132607d1518a4659
lijunyan <li.junyan@microcyber.cn>
2019-05-28 16:50:27 +0800

    愚かなる弟よ......このオレを杀したくば恨め！憎め！そしてみにくく生きのびるがいい.........逃げて...逃げて...生にしがみつくがいい，そして　いつがオレと同じ　眼　をもってオレの前に来い

--- iniesta/src/main/java/com/pw/wispne/iniesta/IniestaApplication.java
+++ iniesta/src/main/java/com/pw/wispne/iniesta/IniestaApplication.java
@@ -3,7 +3,12 @@ package com.pw.wispne.iniesta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 @EnableConfigurationProperties
@@ -11,6 +16,18 @@ public class IniestaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IniestaApplication.class, args);
+	}
+	@Bean
+	public ViewResolver myViewResolver(){
+		return new MyViewResolver();
+	}
+
+	private static class MyViewResolver implements ViewResolver{
+
+		@Override
+		public View resolveViewName(String s, Locale locale) throws Exception {
+			return null;
+		}
 	}
 
 }
--- iniesta/src/main/java/com/pw/wispne/iniesta/helloworld/controller/HelloController.java
+++ iniesta/src/main/java/com/pw/wispne/iniesta/helloworld/controller/HelloController.java
@@ -1,14 +1,23 @@
 package com.pw.wispne.iniesta.helloworld.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Arrays;
+import java.util.Map;
+
+@Controller
 public class HelloController {
-    private String name;
-    @RequestMapping("/sayhello")
-    public String sayHello(){
-        return "Hello "+name;
+
+
+
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+        map.put("hello","<h1>你好</h1>");
+        map.put("users", Arrays.asList("张三","李四","王五"));
+        return "success";
     }
 }
