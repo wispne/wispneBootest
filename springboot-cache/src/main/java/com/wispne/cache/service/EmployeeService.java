4c2509a84bdcc358489b83422c194480b1849ea8
wispne@163.com <54superman>
2019-06-06 07:10:59 +0800

    1111111111

--- springboot-cache/src/main/java/com/wispne/cache/service/EmployeeService.java
+++ springboot-cache/src/main/java/com/wispne/cache/service/EmployeeService.java
@@ -3,13 +3,16 @@ package com.wispne.cache.service;
 import com.wispne.cache.bean.Employee;
 import com.wispne.cache.mapper.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
     @Autowired
     EmployeeMapper employeeMapper;
+    @Cacheable(cacheNames = {"emp"})
     public Employee getEmp(Integer id){
+        System.out.println("查询"+id+"号员工");
         Employee emp= employeeMapper.getEmpById(id);
         return emp;
     }
