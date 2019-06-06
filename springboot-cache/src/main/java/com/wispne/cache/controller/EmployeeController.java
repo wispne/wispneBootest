2a4b103100a9eeb349b783cd6bc8eaf0968b6f6a
lijunyan <li.junyan@microcyber.cn>
2019-06-06 16:33:38 +0800

    11111111111111

--- springboot-cache/src/main/java/com/wispne/cache/controller/EmployeeController.java
+++ springboot-cache/src/main/java/com/wispne/cache/controller/EmployeeController.java
@@ -3,9 +3,7 @@ package com.wispne.cache.controller;
 import com.wispne.cache.bean.Employee;
 import com.wispne.cache.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EmployeeController {
@@ -16,5 +14,15 @@ public class EmployeeController {
         Employee emp = employeeService.getEmp(id);
         return emp;
 
+    }
+    @GetMapping("/emp")
+    public Employee updateEmployee(Employee employee){
+        employeeService.updateEmp(employee);
+        return employee;
+    }
+    @GetMapping("/delemp")
+    public String deleteEmp(Integer id){
+        employeeService.deleteEmp(id);
+        return "success";
     }
 }
--- springboot-cache/src/main/java/com/wispne/cache/service/EmployeeService.java
+++ springboot-cache/src/main/java/com/wispne/cache/service/EmployeeService.java
@@ -3,8 +3,12 @@ package com.wispne.cache.service;
 import com.wispne.cache.bean.Employee;
 import com.wispne.cache.mapper.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class EmployeeService {
@@ -15,5 +19,16 @@ public class EmployeeService {
         System.out.println("查询"+id+"号员工");
         Employee emp= employeeMapper.getEmpById(id);
         return emp;
+
+    }
+    @CachePut(value={"emp"},key="#result.id")
+    public Employee updateEmp(Employee employee){
+        employeeMapper.updateEmployee(employee);
+        return employee;
+    }
+    @CacheEvict(value = "emp")
+    public void deleteEmp(Integer id){
+        System.out.println("");
+        employeeMapper.deleteEmpById(id);
     }
 }
